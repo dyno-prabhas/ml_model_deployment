b93f3f4bf796d09da83fd5b1d61266b8381303f5
prabhas <149877591+dyno-prabhas@users.noreply.github.com>
2025-01-05 09:33:09 +0530

    Update settings.py

--- ml_models/settings.py
+++ ml_models/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--wx)+@m6-q#=t)mnxrl&boyfp6_g5cf*@n$pr@lggg%#02w(d0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['prabhas.pythonanywhere.com', '127.0.0.1', 'prabhasvarma.onrender.com']
 
